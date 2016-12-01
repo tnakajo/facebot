54d8d7727ad6cb00e418742569cd8b7d5e84b660
Taki Nakajo <tnakajo@au1.ibm.com>
2016-12-01 01:03:39 -0600

    bot app

--- app.js
+++ app.js
@@ -1,28 +1,65 @@
-/*eslint-env node*/
-
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
-
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
 var express = require('express');
-
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
-
-// create a new express server
+var request = require('request');
+var bodyParser = require('body-parser');
 var app = express();
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
-
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
-
-// start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+// Server frontpage
+app.get('/', function (req, res) {
+    res.send('This is TestBot Server');
 });
+
+// This code is called only when subscribing the webhook //
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === 'mySecretAccessToken') {
+        res.send(req.query['hub.challenge']);
+    }
+    res.send('Error, wrong validation token');
+})
+
+
+// Incoming messages reach this end point //
+app.post('/webhook/', function (req, res) {
+    messaging_events = req.body.entry[0].messaging;
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i];
+        sender = event.sender.id;
+        if (event.message && event.message.text) {
+            text = event.message.text;
+// Calling the Weather App. Change the address below to the url of your Weather app. Response is sent back to the user via the sendMessage function //
+            request("https://whatistheweather.mybluemix.net/getWeather?text=" + text, function (error, response, body) {
+                sendMessage(sender, body);
+            });
+        }
+    }
+    res.sendStatus(200);
+});
+
+
+// This function receives the response text and sends it back to the user //
+function sendMessage(sender,text) {
+    messageData = {
+        text: text
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: token},
+        method: 'POST',
+        json: {
+            recipient: {id: sender},
+            message: messageData,
+        }
+    }, function (error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+};
+
+var token = "Replace with the access token from Facebook";
+var host = (process.env.VCAP_APP_HOST || 'localhost');
+var port = (process.env.VCAP_APP_PORT || 3000);
+app.listen(port, host);
